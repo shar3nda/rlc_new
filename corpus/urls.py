5e37e1791956f049948c5c6ae13a121142048c36
johann <vyavya2003@yandex.ru>
2023-04-21 13:09:31 +0300

    implement author creation

--- corpus/urls.py
+++ corpus/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from corpus import views
 
 urlpatterns = [
+    path("add-document/", views.add_document, name="add_document"),
     path("documents/", views.documents, name="documents"),
     path("annotate/<int:document_id>/", views.annotate, name="annotate"),
 ]
--- corpus/views.py
+++ corpus/views.py
@@ -1,9 +1,9 @@
-import json
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-from django.http import JsonResponse
-from django.shortcuts import render
-
-from .models import Document, Annotation, Sentence
+from .forms import DocumentForm
+from .models import Document, Sentence, Author
 
 
 # Представление для списка документов
@@ -23,3 +23,47 @@ def annotate(request, document_id):
         "sentences": Sentence.objects.filter(document=doc),
     }
     return render(request, "annotate.html", context)
+
+
+@login_required
+def add_document(request):
+    if request.method == "POST":
+        form = DocumentForm(request.POST)
+        if form.is_valid():
+            # add user to the form
+            form.instance.user = request.user
+
+            # extract the author from the form
+            if form.data["add_to_favorites"] == "on":
+                author = Author.objects.create(
+                    name=form.data["author_name"],
+                    gender=form.data["author_gender"],
+                    program=form.data["author_program"],
+                    language_background=form.data["author_language_background"],
+                    dominant_language=form.data["author_dominant_language"],
+                    language_level=form.data["author_language_level"],
+                    favorite=form.data["add_to_favorites"] == "on",
+                )
+            else:
+                # get an existing author
+                author = Author.objects.get(id=form.data["selected_author"])
+            # add author to the form
+            form.instance.author = author
+            new_document = form.save(commit=False)
+            new_document.user = request.user
+            new_document.save()
+            messages.success(request, "Document added successfully!")
+            return redirect("annotate", document_id=new_document.id)
+        else:
+            messages.error(
+                request,
+                "There was an error adding the document. Please check your input.",
+            )
+    else:
+        form = DocumentForm()
+
+    context = {
+        "form": form,
+        "authors": Author.objects.filter(favorite=True),
+    }
+    return render(request, "add_document.html", context)
