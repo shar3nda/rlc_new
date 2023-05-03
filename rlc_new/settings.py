65e8c3401d319a70383e0d50b1f41aa703963321
johann <vyavya2003@yandex.ru>
2023-05-03 18:53:42 +0300

    redirect to documents list after login

--- rlc_new/settings.py
+++ rlc_new/settings.py
@@ -130,5 +130,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/corpus/documents/"
 LOGOUT_REDIRECT_URL = "/"
