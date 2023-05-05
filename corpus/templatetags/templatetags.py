44d0e0a6426df19cb1a4f5f19ab6e210f2630e13
johann <vyavya2003@yandex.ru>
2023-05-05 15:26:25 +0300

    fix pagination

--- corpus/templatetags/templatetags.py
+++ corpus/templatetags/templatetags.py
@@ -14,3 +14,10 @@ def highlight_search(text, search_text):
     search_text = escape(search_text)
     highlighted = text.replace(search_text, f"<mark>{search_text}</mark>")
     return mark_safe(highlighted)
+
+
+@register.filter
+def update_page_number(query_params, page_number):
+    query_dict = QueryDict(query_params, mutable=True)
+    query_dict['page'] = page_number
+    return query_dict.urlencode()
