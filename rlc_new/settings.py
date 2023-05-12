3382b8fa6b01c7015dacf1d59625ee7fb2d14538
johann <vyavya2003@yandex.ru>
2023-05-12 23:13:20 +0300

    dockerize application

--- rlc_new/settings.py
+++ rlc_new/settings.py
@@ -83,16 +83,29 @@ WSGI_APPLICATION = "rlc_new.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "rlc_new",
-        "USER": "rlc_new",
-        "PASSWORD": "VerySecureUserPassword",
-        "HOST": "localhost",
-        "PORT": "",
+DOCKER = os.environ.get("DOCKER", False)
+if DOCKER:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": "db",
+            "PORT": "5432",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "rlc_new",
+            "USER": "rlc_new",
+            "PASSWORD": "VerySecureUserPassword",
+            "HOST": "localhost",
+            "PORT": "",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
