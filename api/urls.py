1e198a74bbf2ff4938af9ac849c65477e6e77db2
johann <vyavya2003@yandex.ru>
2023-05-18 20:16:46 +0300

    rewrite API using django rest framework

--- api/urls.py
+++ api/urls.py
@@ -1,26 +1,30 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from . import views
+from .views import (
+    AnnotationListCreateViewSet,
+    SentenceViewSet,
+    UserViewSet,
+    auto_annotate,
+    AnnotationRetrieveUpdateDestroyViewSet,
+)
+
+router = DefaultRouter()
+router.register(
+    r"annotations/(?P<sentence_id>\d+)/(?P<alt>\w+)",
+    AnnotationListCreateViewSet,
+    basename="annotations",
+)
+router.register(
+    r"annotations/(?P<guid>\w+)",
+    AnnotationRetrieveUpdateDestroyViewSet,
+    basename="annotations",
+)
+
+router.register(r"get_corrections", SentenceViewSet)
+router.register(r"get_user_info", UserViewSet)
 
 urlpatterns = [
-    path(
-        "annotations/get/<int:sentence_id>/",
-        views.get_sentence_annotations,
-        name="get_sentence_annotations",
-    ),
-    path(
-        "annotations/get/alt/<int:sentence_id>/",
-        views.get_alt_sentence_annotations,
-        name="get_alt_sentence_annotations",
-    ),
-    path("annotations/create/", views.create_annotation, name="create_annotation"),
-    path("annotations/update/", views.update_annotation, name="update_annotation"),
-    path("annotations/delete/", views.delete_annotation, name="delete_annotation"),
-    path(
-        "get_corrections/<int:sentence_id>/",
-        views.get_sentence_corrections,
-        name="get_sentence_corrections",
-    ),
-    path("get_user_info/", views.get_user_info, name="get_user_info"),
-    path("auto_annotate/", views.auto_annotate, name="auto_annotate"),
+    path("", include(router.urls)),
+    path("auto_annotate/", auto_annotate, name="auto_annotate"),
 ]
--- rlc_new/settings.py
+++ rlc_new/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "api.apps.ApiConfig",
     "crispy_forms",
     "crispy_bootstrap5",
+    "rest_framework",
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
