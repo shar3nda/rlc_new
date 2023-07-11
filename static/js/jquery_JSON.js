ce9f33b9fa3726df3f6b1078a4c5eefe42d5d3de
johann <vyavya2003@yandex.ru>
2023-07-11 22:38:04 +0300

    add languages to json correctly

--- static/js/jquery_JSON.js
+++ static/js/jquery_JSON.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
-    $('.lexgram-form').on('submit', function(e) {
-        e.preventDefault();
+$(document).ready(function () {
+  $('.lexgram-form').on('submit', function (e) {
+    e.preventDefault();
 
-        var formData = {};
-        $(this).serializeArray().map(function(item) {
-            var value = item.value;
-            if (item.name.endsWith('[]')) {
-                value = item.value.split('|');
-            }
-            if (formData[item.name]) {
-                if (Array.isArray(formData[item.name])) {
-                    formData[item.name].push(value);
-                } else {
-                    formData[item.name] = [formData[item.name], value];
-                }
-            } else {
-                formData[item.name] = value;
-            }
-        });
-
-        console.log(JSON.stringify(formData));
+    var formData = {};
+    $(this).serializeArray().map(function (item) {
+      var value = item.value;
+      if (item.name !== 'language[]' && item.name.endsWith('[]')) {
+        value = item.value.split('|');
+      }
+      if (formData[item.name]) {
+        if (Array.isArray(formData[item.name])) {
+          formData[item.name].push(value);
+        } else {
+          formData[item.name] = [formData[item.name], value];
+        }
+      } else {
+        formData[item.name] = value;
+      }
     });
+
+    console.log(JSON.stringify(formData));
+  });
 });
