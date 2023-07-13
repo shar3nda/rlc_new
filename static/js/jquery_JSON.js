672f633f863dbd6c197c51a4f4e1d7a405265689
johann <vyavya2003@yandex.ru>
2023-07-13 20:38:58 +0300

    add first search iteration

--- static/js/jquery_JSON.js
+++ static/js/jquery_JSON.js
@@ -5,35 +5,25 @@ $(document).ready(function () {
     var formData = {};
     $(this).serializeArray().map(function (item) {
       var value = item.value;
-      if (!(item.name in ['language[]', 'level[]', 'generallevel[]']) && item.name.endsWith('[]')) {
-        value = item.value.split('|');
+      if (item.name in ['errors[]', 'grammar[]', 'lex[]']) {
+        value = value.split('|');
       }
-      if (formData[item.name]) {
-        if (Array.isArray(formData[item.name])) {
-          formData[item.name].push(value);
-        } else {
-          formData[item.name] = [formData[item.name], value];
+      if (item.name.endsWith('[]')) {
+        if (!(item.name in formData)) {
+          formData[item.name] = [];
         }
+        formData[item.name].push(value);
       } else {
         formData[item.name] = value;
       }
     });
 
-    var $form = $('<form/>', {
-      'action': this.action,
-      'method': 'post',
-      'style': 'display:none;'
-    });
+    // encode formData as GET parameters
+    var queryString = Object.keys(formData).map(function (key) {
+      return encodeURIComponent(key) + '=' + encodeURIComponent(formData[key]);
+    }).join('&');
 
-    $.each(formData, function(name, value) {
-      $form.append($('<input/>', {
-        'type': 'hidden',
-        'name': name,
-        'value': value
-      }));
-    });
-
-    $('body').append($form);
-    $form.submit();
+    // go to the URL
+    window.location.href = `/corpus/search_results/?${queryString}`;
   });
 });
