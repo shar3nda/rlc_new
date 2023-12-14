c1b86b64d69819e97df6e43125c599843a821212
Ivan Krupnov <ivankrup2003@gmail.com>
2023-12-14 18:18:47 +0300

    update settings for deploy

--- rlc_new/settings.py
+++ rlc_new/settings.py
@@ -172,6 +172,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = "/var/www/rlc_new/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
